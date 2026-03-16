86ec2b115669066e136df506ccb78c7a01049747
Leidy <leidyjohanaalomiazapata2@gmail.com>
2026-03-16 18:24:30 -0500

    Cambio de puerto a 5005 para evitar conflictos con usuarios

--- src/app.js
+++ src/app.js
@@ -2,27 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
+const peliculaRoutes = require('./routes/pelicularoute'); 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-const Pelicula = require('./models/peliculamodel');
+
+app.use('/api/peliculas', peliculaRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Conectado exitosamente a MongoDB Atlas'))
     .catch((error) => console.error('Error al conectar a MongoDB:', error));
-
-// Ruta de prueba para ver si los datos fluyen
-app.get('/probar-datos', async (req, res) => {
-    try {
-        const unaPelicula = await Pelicula.findOne();
-        res.json({
-            mensaje: "Conexión real establecida",
-            dato: unaPelicula
-        });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
+       
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
